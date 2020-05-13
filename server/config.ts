cab0fd72423448c0e56ba40567342d6343e32aa1
Timeless Ventures <ganeshmani009@gmail.com>
2020-05-13 15:35:57 +0530

    changes

--- server/config.ts
+++ server/config.ts
@@ -1,15 +1,27 @@
 const config = {
-    server: {
-        sessionSecret: process.env.SESSION_SECRET,
-        port: process.env.PORT,
-        mountPath: process.env.MOUNT_PATH
-    },
-    spotify: {
-        clientId: process.env.SPOTIFY_CLIENT_ID,
-        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-        scopes: ['user-library-read', 'playlist-read-private', 'playlist-read-collaborative', 'user-read-private', 'user-read-email', 'user-follow-read', 'user-top-read', 'user-read-playback-state'],
-        redirectUri: process.env.SPOTIFY_REDIRECT_URI
-    }
+  server: {
+    sessionSecret: process.env.SESSION_SECRET || "charly",
+    port: process.env.PORT || 9999,
+    mountPath: process.env.MOUNT_PATH || "http://localhost:9999",
+  },
+  spotify: {
+    clientId:
+      process.env.SPOTIFY_CLIENT_ID || "c83bb6bf62eb42229f300aa78243cb1a",
+    clientSecret:
+      process.env.SPOTIFY_CLIENT_SECRET || "98b2c12e46134f4b9fdd0847572727e6",
+    scopes: [
+      "user-library-read",
+      "playlist-read-private",
+      "playlist-read-collaborative",
+      "user-read-private",
+      "user-read-email",
+      "user-follow-read",
+      "user-top-read",
+      "user-read-playback-state",
+    ],
+    redirectUri:
+      process.env.SPOTIFY_REDIRECT_URI || "http://localhost:9999/auth/callback",
+  },
 };
 
 export { config };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,32 +1,38 @@
-import * as express from 'express';
-import * as SpotifyGraphQL from 'spotify-graphql';
-import * as fs from 'fs';
-import * as path from 'path';
-import { GraphQLSchema } from 'graphql';
-import { config } from '../config';
-import * as ExpressGraphQL from 'express-graphql';
-import * as bodyParser from 'body-parser';
-import * as cookieParser from 'cookie-parser';
-import * as methodOverride from 'method-override';
-import * as session from 'express-session';
-import * as passport from 'passport';
-import * as timeout from 'connect-timeout';
-const SpotifyStrategy: any = require('passport-spotify').Strategy;
+import * as express from "express";
+import * as SpotifyGraphQL from "spotify-graphql";
+import * as fs from "fs";
+import * as path from "path";
+import { GraphQLSchema } from "graphql";
+import { config } from "../config";
+import * as ExpressGraphQL from "express-graphql";
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+import * as methodOverride from "method-override";
+import * as session from "express-session";
+import * as passport from "passport";
+import * as timeout from "connect-timeout";
+import * as cors from "cors";
+const SpotifyStrategy: any = require("passport-spotify").Strategy;
 
 const PORT = config.server.port;
 
-let spotifyStrategy = new SpotifyStrategy({
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+let spotifyStrategy = new SpotifyStrategy(
+  {
     clientID: config.spotify.clientId,
     clientSecret: config.spotify.clientSecret,
     callbackURL: config.spotify.redirectUri,
-    passReqToCallback: true
+    passReqToCallback: true,
   },
-  function(request, accessToken, refreshToken, profile, done) {
+  function (request, accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
       done(null, Object.assign({}, profile, { accessToken }));
     });
   }
- );
+);
 
 passport.use(spotifyStrategy);
 
@@ -45,52 +51,54 @@ app.use(methodOverride());
 app.use(session({ secret: config.server.sessionSecret }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.resolve(__dirname, '../../../node_modules/')));
-app.use(express.static(path.resolve(__dirname, '../../../client/')));
-app.use(timeout('5s'));
+app.use(express.static(path.resolve(__dirname, "../../../node_modules/")));
+app.use(express.static(path.resolve(__dirname, "../../../client/")));
+app.use(timeout("5s"));
+app.use(cors());
 
 // AUTH
-app.get('/auth/connect',
-  passport.authenticate('spotify', {scope: config.spotify.scopes}),
+app.get(
+  "/auth/connect",
+  passport.authenticate("spotify", { scope: config.spotify.scopes }),
   (req: any, res) => {
-  // The request will be redirected to spotify for authentication, so this
-  // function will not be called.
-});
-
-app.get('/auth/callback',
-  passport.authenticate('spotify'),
-  (req: any, res) => {
-    res.redirect(config.server.mountPath);
+    // The request will be redirected to spotify for authentication, so this
+    // function will not be called.
   }
 );
 
+app.get("/auth/callback", passport.authenticate("spotify"), (req: any, res) => {
+  res.redirect(config.server.mountPath);
+});
 
-app.get('/auth/logout', (req: any, res) => {
+app.get("/auth/logout", (req: any, res) => {
   req.logout();
   res.redirect(config.server.mountPath);
 });
 
-app.post('/graphql', (req: any, res: any) => {
-  let accessToken = (req.user && req.user.accessToken) || '';
-  let schema = SpotifyGraphQL.getSchema(Object.assign({}, config.spotify, {
-    accessToken: accessToken
-  }));
+app.post("/graphql", (req: any, res: any) => {
+  let accessToken = (req.user && req.user.accessToken) || "";
+  let schema = SpotifyGraphQL.getSchema(
+    Object.assign({}, config.spotify, {
+      accessToken: accessToken,
+    })
+  );
   return ExpressGraphQL({
     schema: schema,
-    graphiql: true
+    graphiql: true,
   })(req, res);
 });
 
-app.get('/', (req: any, res) => {
-  res.set('Content-Type', 'text/html');
-  res.send(new Buffer(fs.readFileSync(path.resolve(__dirname, '../../../client/index.html'))));
+// app.get("/", (req: any, res) => {
+//   res.set("Content-Type", "text/html");
+//   res.send(
+//     new Buffer(
+//       fs.readFileSync(path.resolve(__dirname, "../../../client/index.html"))
+//     )
+//   );
+// });
+// if (config.server.mountPath) {
+//   appOnMountPath.use(config.server.mountPath, app);
+// }
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
-
-if (config.server.mountPath) {
-  appOnMountPath.use(
-    config.server.mountPath,
-    app
-  )
-}
-console.log(`graphql server listening on port ${PORT}`);
-(config.server.mountPath ? appOnMountPath : app).listen(PORT);
